cfe3bd2ee231c55682569fe3c14662087388a813
gustavoghp87 <ghp.2120@gmail.com>
2020-11-19 01:31:35 -0300

    cambio servidor emergencia

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@ const local = false
 
 export let SERVER:string
 if (local) SERVER = "http://localhost:4005"
-else SERVER = "https://registros-rest-api.herokuapp.com"
+else SERVER = "https://registros-rest-api2.herokuapp.com"
 
 console.log(SERVER)
