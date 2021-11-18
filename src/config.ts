81578a0662776d888a7a93c549ab777cbbbc4c6d
Gustavo Hernán Perrupato <ghp.2120@gmail.com>
2021-11-17 23:26:54 -0300

    upgrade, removed redux and auth

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const recaptchaPublicKey = "6LfDIdIZAAAAAElWChHQZq-bZzO9Pu42dt9KANY9"
 
-const local = true
+const local = false
 export let SERVER: string = local ? "http://localhost:4005" : "https://registros-rest-api.herokuapp.com"
