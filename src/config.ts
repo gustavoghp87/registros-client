c7e276a7179c784bf6d391773a228fc026aeb502
gustavoghp87 <ghp.2120@gmail.com>
2023-07-29 21:07:30 -0300

    Corregido dominio api

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,12 @@ const isLocalhost: boolean = Boolean(
     window.location.hostname === 'localhost' ||
     window.location.hostname === '[::1]' ||
     window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
-)
+);
 
 export const SERVER: string = (isLocalhost ?
     "http://localhost:4005"
     :
-    "https://misericordiawebapi-ghp2120.b4a.run/" // production server will not respond because cors policies
+    "https://misericordiawebapi-ghp2120.b4a.run" // production server will not respond because cors policies
 );
 
 export const pointer = {
